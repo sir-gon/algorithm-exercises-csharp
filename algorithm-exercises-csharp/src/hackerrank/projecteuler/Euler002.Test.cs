ce45cf52cad7c49a933ac6259655f3849af5f718
Gonzalo Diaz <devel@gon.cl>
2024-05-08 18:58:24 -0400

    [Hacker Rank]: Project Euler #2: Even Fibonacci numbers solved ✓

---
+++ algorithm-exercises-csharp/src/hackerrank/projecteuler/Euler002.Test.cs
@@ -0,0 +1,27 @@
+namespace algorithm_exercises_csharp.hackerrank.prohecteuler;
+
+[TestClass]
+public class Euler002Test
+{
+  public class Euler002TestCase {
+    public int n; public int answer;
+  }
+
+  // dotnet_style_readonly_field = true
+  private static readonly Euler002TestCase[] tests = [
+    new() { n = 10, answer = 10},
+    new() { n = 100, answer = 44}
+  ];
+
+  [TestMethod]
+  public void Euler002ProblemTest()
+  {
+    int result;
+
+    foreach (Euler002TestCase test in tests) {
+      result = Euler002Problem.Euler002(test.n);
+        Assert.AreEqual(test.answer, result);
+    }
+  }
+}
+
---
+++ algorithm-exercises-csharp/src/hackerrank/projecteuler/Euler002.cs
@@ -0,0 +1,34 @@
+namespace algorithm_exercises_csharp.hackerrank.prohecteuler;
+
+using System.Diagnostics.CodeAnalysis;
+
+public class Euler002Problem
+{
+  [ExcludeFromCodeCoverage]
+  protected Euler002Problem() {}
+
+  public static int FiboEvenSum(int n) {
+    int total = 0;
+    int fibo;
+    int fibo1 = 1;
+    int fibo2 = 1;
+
+    while (fibo1 + fibo2 < n) {
+        fibo = fibo1 + fibo2;
+        fibo1 = fibo2;
+        fibo2 = fibo;
+
+        if(fibo % 2 == 0) {
+            total += fibo;
+        }
+    }
+
+    return total;
+  }
+
+  // Function to find the sum of all multiples of a and b below n
+  public static int Euler002(int n)
+  {
+    return FiboEvenSum(n);
+  }
+}

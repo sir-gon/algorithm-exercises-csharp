490a9988c6d7a245345623b3bcf94009cce8a169
Gonzalo Diaz <devel@gon.cl>
2024-05-15 19:30:30 -0400

    [Hacker Rank]: Solve Me First solved ✓

---
+++ algorithm-exercises-csharp/src/hackerrank/warmup/SolveMeFirst.cs
@@ -0,0 +1,15 @@
+namespace algorithm_exercises_csharp;
+
+using System.Diagnostics.CodeAnalysis;
+
+public class SolveMeFirst
+{
+  [ExcludeFromCodeCoverage]
+  protected SolveMeFirst() { }
+
+  public static int solveMeFirst(int _a, int _b)
+  {
+    return _a + _b;
+  }
+
+}

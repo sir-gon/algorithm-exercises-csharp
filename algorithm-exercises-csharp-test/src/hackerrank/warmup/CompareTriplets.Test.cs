54cb4ef5f85f36236fbb1a30d5a5ee32d87c5a04
Gonzalo Diaz <devel@gon.cl>
2024-05-17 14:07:22 -0400

    [Hacker Rank]: Compare Triplets solved ✓

---
+++ algorithm-exercises-csharp-test/src/hackerrank/warmup/CompareTriplets.Test.cs
@@ -0,0 +1,37 @@
+namespace algorithm_exercises_csharp.hackerrank;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class CompareTripletsTest
+{
+  public class CompareTripletsTestCase
+  {
+    public List<int> a = [];
+    public List<int> b = [];
+    public List<int> expected = [];
+  }
+
+  // dotnet_style_readonly_field = true
+  private static readonly CompareTripletsTestCase[] tests = [
+    new()
+    {
+      a = [5, 6, 7],
+      b = [3, 6, 10],
+      expected = [1, 1]
+    }
+  ];
+
+  [TestMethod]
+  public void testSimpleArraySum()
+  {
+    List<int> result;
+
+    foreach (CompareTripletsTestCase test in tests)
+    {
+      result = CompareTriplets.compareTriplets(test.a, test.b);
+      CollectionAssert.AreEquivalent(test.expected, result);
+    }
+  }
+}
+

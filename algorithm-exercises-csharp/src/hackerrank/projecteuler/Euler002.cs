fa9541c7dd9b59df45b1495621345e6e9bb83fa9
Gonzalo Diaz <devel@gon.cl>
2024-06-20 12:44:35 -0400

    [Hacker Rank]: Project Euler #2: Even Fibonacci numbers. Some logs added.

--- algorithm-exercises-csharp/src/hackerrank/projecteuler/Euler002.cs
+++ algorithm-exercises-csharp/src/hackerrank/projecteuler/Euler002.cs
@@ -11,7 +11,10 @@ public class Euler002
 
   public static int fiboEvenSum(int n)
   {
-    int total = 0;
+    Log.info("fiboEvenSum({n})", n);
+
+    int i = 0;
+    int even_sum = 0;
     int fibo;
     int fibo1 = 1;
     int fibo2 = 1;
@@ -19,16 +22,22 @@ public class Euler002
     while (fibo1 + fibo2 < n)
     {
       fibo = fibo1 + fibo2;
-      fibo1 = fibo2;
-      fibo2 = fibo;
+
+      Log.info("Fibonacci({i}) = {fibo}", i, fibo);
 
       if (fibo % 2 == 0)
       {
-        total += fibo;
+        even_sum += fibo;
       }
+
+      fibo1 = fibo2;
+      fibo2 = fibo;
+      i += 1;
     }
 
-    return total;
+    Log.info("Problem 0002 result: {even_sum}", even_sum);
+
+    return even_sum;
   }
 
   // Function to find the sum of all multiples of a and b below n

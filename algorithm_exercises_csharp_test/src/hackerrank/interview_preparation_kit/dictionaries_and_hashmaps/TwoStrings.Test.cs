ad7622d6a3a3fcfe85b78ddfc4415ef96a066687
Gonzalo Diaz <devel@gon.cl>
2024-09-03 12:58:03 -0400

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Two Strings. Load test case data from JSON.
    JSON load as Resource: https://khalidabuhakmeh.com/how-to-use-embedded-resources-in-dotnet

--- algorithm_exercises_csharp_test/src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/TwoStrings.Test.cs
+++ algorithm_exercises_csharp_test/src/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/TwoStrings.Test.cs
@@ -5,42 +5,31 @@ public class TwoStringsTest
 {
   public class TwoStringsTestCase
   {
-    public string title = "";
-    public string s1 = "";
-    public string s2 = "";
-    public string expected = "Yes";
+    public string title { get; set; } = default!;
+
+    public string s1 { get; set; } = default!;
+
+    public string s2 { get; set; } = default!;
+
+    public string expected { get; set; } = default!;
   }
 
-  private static readonly TwoStringsTestCase[] tests = [
-    new()
-    {
-      title = "Example 1",
-      s1 = "and",
-      s2 = "art",
-      expected = "Yes"
-    },
-    new()
-    {
-      title = "Example 2",
-      s1 = "be",
-      s2 = "cat",
-      expected = "No"
-    },
-    new()
-    {
-      title = "Sample Test Case 0",
-      s1 = "hello",
-      s2 = "world",
-      expected = "Yes"
-    },
-  ];
+  private List<TwoStringsTestCase> testCases { get; set; } = default!;
+
+  [TestInitialize]
+  public void testInitialize()
+  {
+    testCases = JsonLoader.resourceLoad<List<TwoStringsTestCase>>(
+      "hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/two_strings.testcases.json"
+    ) ?? [];
+  }
 
   [TestMethod]
   public void testTwoStrings()
   {
     string result;
 
-    foreach (TwoStringsTestCase test in tests)
+    foreach (TwoStringsTestCase test in testCases)
     {
       result = TwoStrings.twoStrings(test.s1, test.s2);
       Assert.AreEqual(test.expected, result);

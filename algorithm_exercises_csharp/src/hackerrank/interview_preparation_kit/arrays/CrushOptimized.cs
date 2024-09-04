cda1afda7206288e64d22db2dd05b4d359c99ba2
Gonzalo Diaz <devel@gon.cl>
2024-09-04 12:00:15 -0400

    [Hacker Rank] Interview Preparation Kit: Arrays: Array Manipulation. Solved ✅.

---
+++ algorithm_exercises_csharp/src/hackerrank/interview_preparation_kit/arrays/CrushOptimized.cs
@@ -0,0 +1,49 @@
+/**
+ * Crush (Optimized).
+ *
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/arrays/crush.md]]
+ * @link Solution notes [[docs/hackerrank/interview_preparation_kit/arrays/crush_optimized-solution-notes.md]]
+ */
+
+namespace algorithm_exercises_csharp.hackerrank.interview_preparation_kit;
+
+using System.Diagnostics.CodeAnalysis;
+
+public class CrushOptimized
+{
+  [ExcludeFromCodeCoverage]
+  private CrushOptimized() { }
+
+  /**
+   * arrayManipulation.
+   */
+  public static long arrayManipulation(int n, List<List<int>> queries)
+  {
+    // why adding 2?
+    //   first slot to adjust 1-based index and
+    //   last slot for storing accumSum result
+    int[] result = new int[n + 2];
+    Array.Fill(result, 0);
+    int maximum = 0;
+
+    foreach (List<int> query in queries)
+    {
+      int a = query[0];
+      int b = query[1];
+      int k = query[2];
+
+      // Prefix
+      result[a] += k;
+      result[b + 1] -= k;
+
+      int accumSum = 0;
+      foreach (int value in result)
+      {
+        accumSum += value;
+        maximum = Math.Max(maximum, accumSum);
+      }
+    }
+
+    return maximum;
+  }
+}

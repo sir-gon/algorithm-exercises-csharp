7cf0d685492c802cb6fe66d9815744c47c1186d7
Gonzalo Diaz <devel@gon.cl>
2025-02-16 16:35:22 -0300

    [Hacker Rank] Interview Preparation Kit: Arrays: New Year Chaos. Solved ✓.

---
+++ algorithm_exercises_csharp_test/src/hackerrank/interview_preparation_kit/arrays/NewYearChaos.Test.cs
@@ -0,0 +1,37 @@
+namespace algorithm_exercises_csharp.hackerrank.interview_preparation_kit;
+
+[TestClass]
+public class NewYearChaosTest
+{
+  public class NewYearChaosTestCase
+  {
+    public string title { get; set; } = default!;
+    public List<int> input { get; set; } = default!;
+    public string expected { get; set; } = default!;
+  }
+
+  private List<NewYearChaosTestCase> testCases { get; set; } = default!;
+
+  [TestInitialize]
+  public void testInitialize()
+  {
+    testCases = JsonLoader.resourceLoad<List<NewYearChaosTestCase>>(
+      "hackerrank/interview_preparation_kit/arrays/new_year_chaos.testcases.json"
+    ) ?? [];
+  }
+
+  [TestMethod]
+  public void testMinimumBribesText()
+  {
+    string result;
+
+    foreach (NewYearChaosTestCase test in testCases)
+    {
+      result = NewYearChaos.minimumBribesText(test.input);
+      NewYearChaos.minimumBribes(test.input);
+
+      Assert.AreEqual(test.expected, result);
+    }
+  }
+}
+

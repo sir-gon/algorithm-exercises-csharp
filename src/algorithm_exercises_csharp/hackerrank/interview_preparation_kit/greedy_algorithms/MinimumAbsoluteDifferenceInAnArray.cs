fd9155da460081de56242299a51aae1a943cf15a
Gonzalo Diaz <devel@gon.cl>
2025-05-12 12:45:57 -0400

    [Hacker Rank] Interview Preparation Kit: Greedy Algorithms: Minimum Absolute Difference in an Array. Solved ✓.

---
+++ src/algorithm_exercises_csharp/hackerrank/interview_preparation_kit/greedy_algorithms/MinimumAbsoluteDifferenceInAnArray.cs
@@ -0,0 +1,45 @@
+// @link Problem definition [[docs/hackerrank/interview_preparation_kit/greedy_algorithms/minimum-absolute-difference-in-an-array.md]]
+
+namespace algorithm_exercises_csharp.hackerrank.interview_preparation_kit.greedy_algorithms;
+
+using System.Diagnostics.CodeAnalysis;
+
+public class MinimumAbsoluteDifferenceInAnArray
+{
+  [ExcludeFromCodeCoverage]
+  private MinimumAbsoluteDifferenceInAnArray()
+  {
+  }
+
+  /**
+   * minimumAbsoluteDifference.
+   */
+  public static int minimumAbsoluteDifference(List<int> arr)
+  {
+    List<int> sortedNums = [.. arr.ConvertAll(x => x).OrderBy(x => x).ToList()];
+
+    // Find the minimum absolute difference
+    int result = 0;
+    bool resultEmpty = true;
+
+    for (int i = 0; i < sortedNums.Count - 1; i++)
+    {
+      int aValue = sortedNums[i];
+      int bValue = sortedNums[i + 1];
+
+      int diff = Math.Abs(aValue - bValue);
+
+      if (resultEmpty)
+      {
+        result = diff;
+        resultEmpty = false;
+      }
+      else
+      {
+        result = Math.Min(result, diff);
+      }
+    }
+
+    return result;
+  }
+}

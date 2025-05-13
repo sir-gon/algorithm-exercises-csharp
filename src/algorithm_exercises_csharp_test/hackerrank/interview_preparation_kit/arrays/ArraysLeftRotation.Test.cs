b9da31fc0f5a1d042b9a57a41519fb11cb485e6b
Gonzalo Diaz <devel@gon.cl>
2025-05-13 12:30:04 -0400

    [REFACTOR] [Hacker Rank] Interview Preparation Kit: Arrays: Left Rotation. Testing way changed.

--- src/algorithm_exercises_csharp_test/hackerrank/interview_preparation_kit/arrays/ArraysLeftRotation.Test.cs
+++ src/algorithm_exercises_csharp_test/hackerrank/interview_preparation_kit/arrays/ArraysLeftRotation.Test.cs
@@ -5,43 +5,21 @@ using algorithm_exercises_csharp.hackerrank.interview_preparation_kit.arrays;
 [TestClass]
 public class ArraysLeftRotationTest
 {
-  public class ArraysLeftRotationTestCase
-  {
-    public List<int> input { get; set; } = default!;
-    public List<int> expected { get; set; } = default!;
-  }
-
   public class ArraysLeftRotationsTestCase
   {
-    public List<int> input { get; set; } = default!;
+    public List<int> input { get; } = default!;
     public int d { get; set; }
-    public List<int> expected { get; set; } = default!;
+    public List<int> expected { get; } = default!;
   }
 
-  private List<ArraysLeftRotationTestCase> testCases { get; set; } = default!;
-
-  private static readonly ArraysLeftRotationsTestCase[] testRotationsCases = [
-    new() { input = [1, 2, 3, 4, 5], d = 4, expected = [5, 1, 2, 3, 4] }
-  ];
+  private List<ArraysLeftRotationsTestCase> testCases = default!;
 
   [TestInitialize]
   public void testInitialize()
   {
-    testCases = JsonLoader.resourceLoad<List<ArraysLeftRotationTestCase>>(
+    testCases = JsonLoader.resourceLoad<List<ArraysLeftRotationsTestCase>>(
       "hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.testcases.json"
     ) ?? [];
-  }
-
-  [TestMethod]
-  public void testRotLeftOne()
-  {
-    List<int> result;
-
-    foreach (ArraysLeftRotationTestCase test in testCases)
-    {
-      result = ArraysLeftRotation.rotLeftOne(test.input);
-      CollectionAssert.AreEquivalent(test.expected, result);
-    }
   }
 
   [TestMethod]
@@ -49,7 +27,7 @@ public class ArraysLeftRotationTest
   {
     List<int> result;
 
-    foreach (ArraysLeftRotationsTestCase test in testRotationsCases)
+    foreach (ArraysLeftRotationsTestCase test in testCases)
     {
       result = ArraysLeftRotation.rotLeft(test.input, test.d);
       CollectionAssert.AreEquivalent(test.expected, result);
